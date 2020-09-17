f4b5d7c6b80b28d9229524efd0f188460f627a6b
MugishaU <muwiragiye123@gmail.com>
2020-09-17 18:07:26 +0100

    add protected routes

--- library/library_app/views.py
+++ library/library_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
 from library_app.models import Book as Books
 
+@login_required
 def index(req):
     context = {'books': Books.objects.all()}
     return render(req, 'library_app/index.html', context )
 
+@login_required
 def show(req, id):
     context = {'book': Books.objects.get(pk=id)}
     return render(req, 'library_app/show.html', context )
