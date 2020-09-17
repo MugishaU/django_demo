ca666cc2ffb2ce4a57525770c7f543670dcfaa45
MugishaU <muwiragiye123@gmail.com>
2020-09-17 17:40:40 +0100

    add login & logout

--- library/library/urls.py
+++ library/library/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library_app.urls')),
-    path('signup/', user_views.signup, name="signup")
+    path('signup/', user_views.signup, name = "signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
 
 handler404 = 'library_app.views.not_found_404'
