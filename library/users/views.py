709c600fdadfd6ef7a86b40728331afa7b48981a
MugishaU <muwiragiye123@gmail.com>
2020-09-17 11:44:41 +0100

    create user form view

--- library/users/views.py
+++ library/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+form django.contrib import messages
+from forms import UserSignupForm
 
 # Create your views here.
+def register(req):
+    if req.method == 'POST':
+        form = UserSignupForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(req, f'Welcome to the library, {username}!')
+            return redirect('library_app.index')
+        else:
+            form = UserSignupForm()
+            data = {'form': form}
+            return render(req, 'users/signup.html', data) #CHECK CHECK CHECK
