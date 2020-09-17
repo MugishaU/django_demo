60850080e348a6cbfee6afd41dfbc9d8c65b6c89
MugishaU <muwiragiye123@gmail.com>
2020-09-17 11:26:38 +0100

    rename model import from Book to Books

--- library/library_app/views.py
+++ library/library_app/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from library_app.models import Book
+from library_app.models import Book as Books
 
 def index(req):
-    context = {'books': Book.objects.all()}
+    context = {'books': Books.objects.all()}
     return render(req, 'library_app/index.html', context )
 
 def show(req, id):
-    context = {'book': Book.objects.get(pk=id)}
+    context = {'book': Books.objects.get(pk=id)}
     return render(req, 'library_app/show.html', context )
 
 def not_found_404(req, exception):
