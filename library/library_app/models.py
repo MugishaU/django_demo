af58ef22196c5f2b1ba07f1bea51674382619184
MugishaU <muwiragiye123@gmail.com>
2020-09-21 11:39:51 +0100

    add borrower to books

--- library/library_app/models.py
+++ library/library_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -10,6 +11,6 @@ class Author(models.Model):
 class Book(models.Model):
     title= models.CharField(max_length=40, null=True)
     author= models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
-
+    borrower= models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return self.title
